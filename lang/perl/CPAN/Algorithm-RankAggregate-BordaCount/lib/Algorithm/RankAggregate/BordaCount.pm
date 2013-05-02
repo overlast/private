744be3e2669518b8a76e97843a16cfdd1ea23315
Toshinori Sato <overlast@nhn.com>
2013-05-02 20:05:48 +0900

    Initial commit

---
+++ lang/perl/CPAN/Algorithm-RankAggregate-BordaCount/lib/Algorithm/RankAggregate/BordaCount.pm
@@ -0,0 +1,32 @@
+package Algorithm::RankAggregate::BordaCount;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Algorithm::RankAggregate::BordaCount -
+
+=head1 SYNOPSIS
+
+  use Algorithm::RankAggregate::BordaCount;
+
+=head1 DESCRIPTION
+
+Algorithm::RankAggregate::BordaCount is
+
+=head1 AUTHOR
+
+Toshinori Sato E<lt>overlasting {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

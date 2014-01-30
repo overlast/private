90aa27a0d228bb6546c0a19b223c7017431060ee
Toshinori Sato <overlasting@gmail.com>
2014-01-30 18:42:10 +0900

    To use k2pdfopt

---
+++ platform/k2pdfopt/bin/do_k2pdfopt_for_kpw.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x # 実行されたコマンドを表示する
+set -e # エラーがあったら諦めろ
+
+BASEDIR=`cd $(dirname $0); pwd`
+
+PDF_PATH=$1
+
+if [ -a ${PDF_PATH} ]; then
+    k2pdfopt ${PDF_PATH} -ocr -ocrlang eng -dev kpw -bp -f2p -1
+fi

85c90fdcd3657ed0eb194eee0f152f4d0581bad7
Toshinori Sato (@overlast) <overlasting@gmail.com>
2014-05-22 22:54:08 +0900

    something chenge

--- conf/centos6/chfef/sh/make_chef_env.sh
+++ conf/centos6/chfef/sh/make_chef_env.sh
@@ -7,5 +7,26 @@ BASEDIR=`cd $(dirname $0); pwd`
 USER_ID=`/usr/bin/id -u`
 
 sudo yum install ruby ruby-devel rubygems make gcc
-gem install chef # 1回で入りきらない
-gem install chef # 2回目でsuccess
+
+if hash chef-solo 2>/dev/null; then
+    echo "chef is already installed"
+else
+    gem install chef # 1回で入りきらない
+    gem install chef # 2回目でsuccess
+fi
+
+if hash rbenb 2>/dev/null; then
+    rbenv rehash
+fi
+
+if [ -e $HOME/.chef ]; then
+    echo "knife is already configured"
+else
+    knife configure
+fi
+
+gem install knife-solo
+
+if hash rbenb 2>/dev/null; then
+    rbenv rehash
+fi
